0cb8efd11ea58502fa25057e298a277977f14f5a
mpmiszczyk <mpmiszczyk@gmail.com>
2025-09-02 22:54:57 +0200

    stage 02: respond to PING

--- app/main.go
+++ app/main.go
@@ -21,9 +21,14 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	fmt.Println("before Accept()")
+	conn, err := l.Accept()
+	fmt.Println("after Accept()")
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	fmt.Println("before PONG")
+	conn.Write([]byte("+PONG\r\n"))
+	fmt.Println("after PONG")
 }
